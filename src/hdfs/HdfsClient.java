14611379e833226efbb91a4fbd968dd774eff8e7
AliAITBELAISSAOUI <57939088+AliAITBELAISSAOUI@users.noreply.github.com>
2020-01-15 17:38:04 +0100

    Add files via upload

---
+++ src/hdfs/HdfsClient.java
@@ -0,0 +1,201 @@
+package hdfs;
+
+import java.io.*;
+import java.net.Socket;
+import formats.Format;
+import formats.KV;
+import formats.KVFormat;
+import formats.LineFormat;
+
+public class HdfsClient {
+
+    private static int numPorts[] = {7650, 7654, 3478, 5481, 7193};
+    private static String nomMachines[] = {"aymen, ali, luc, sherwin, khalil"};
+    final static Long nbServers = 5;
+    private static long taille_fragment = 185;
+    private static KV cst = new KV("hi","hello");
+
+    private static void usage() {
+        System.out.println("Usage: java HdfsClient read <file>");
+        System.out.println("Usage: java HdfsClient write <line|kv> <file>");
+        System.out.println("Usage: java HdfsClient delete <file>");
+    }
+	
+    public static void HdfsDelete(String hdfsFname) {
+        try{
+        	int j ;
+        	
+            // supprimer les fichiers générés des serveurs
+            
+            for (int i = 0; i < nbfragments; i++) {
+            	j = i % nbServers; 
+                Socket sock = new Socket (nomMachines[j], numPorts[j]);
+                ObjectOutputStream objectOS = new ObjectOutputStream(sock.getOutputStream());
+                objectOS.writeObject(Commande.CMD_DELETE.toString() + " " + hdfsFname + Integer.toString(i));
+                objectOS.close();
+                sock.close();                
+            }
+        	
+        	//supprimer les info de hdfsFname du fichier node.
+        	
+        	LineFormat node = new LineFormat("../config/node.txt");
+        	node.open();
+        	
+        	int index = 0;
+        	int indexancient = 0;
+        	KV buffer = cst;
+            while (buffer != null){
+            	indexancient = index;
+                buffer = fichier.read();
+                String[] lis = (buffer.v).split(":");
+                index = fichier.getIndex();
+                if (lis[0] == hdfsFname) {
+                	//supprimer la ligne contenant ce nom
+                	buffer = null;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void HdfsWrite(Type fmt, String localFSSourceFname, int repFactor) {
+         try {
+            
+            // créer les constantes du problème.
+            
+            String fragment;
+            int index ;
+            KV buffer = new KV();
+            
+            // calculer la taille du fichier
+            
+        	File file = new File(localFSSourceFname);
+        	long taille = file.getTotalSpace();
+        	
+        	// vérifier que la taille du bloc est un diviseur de la taille totale sinon ajouter 1.
+        	
+        	int nbfragments = taille/taille_fragment;
+        	if (taille%taille_fragment != 0) { nbfragments ++;}
+            
+        	// ajouter le nombre de fragments dans le fichier node.
+        	
+        	FileWriter node = new Filewriter("../config/node.txt");
+        	node.write(localFSSourceFname+":"+Integer.toString(nbfragments)+"\n");
+        	node.close();
+            
+        	// le cas d'un lineformat fichier:
+        	
+            if (fmt == Type.LINE){
+            	
+            	
+                LineFormat fichier = new LineFormat(localFSSourceFname);
+                fichier.open(Format.OpenMode.R);
+                
+                for (int i=0; i < nbfragments; i++){
+                    
+                	index = 0;
+                    buffer = cst ;
+                    
+                    //créer le fragment à partir des lignes:
+                    
+                    fragment = "";
+                    while (index < taille_fragment && buffer != null){
+                        buffer = fichier.read();
+                        fragment = fragment + buffer.v;
+                        index = fichier.getIndex()-i*nbfragments;
+                    }
+                    
+                    int t = i%nbServers;
+                    
+                    Socket socket = new Socket (nomMachines[t], numPorts[t]);
+                    ObjectOutputStream objectOS = new ObjectOutputStream(socket.getOutputStream());
+                    objectOS.writeObject(Commande.CMD_WRITE.toString() + "/@/" + localFSSourceFname + "_" + Integer.toString(i) + "/@/" + fragment);
+                    objectOS.close();
+                    socket.close();
+                }
+                fichier.close();
+            }else if (fmt == formats.Type.KV){
+            
+            KVFormat fichier = new KVFormat(localFSSourceFname);
+            fichier.open(Format.OpenMode.R);
+            
+            for (int i=0; i < nbfragments ; i++){
+                index = 0;
+                buffer =  cst;
+                while (index < taillebloc && buffer != null){
+                    buffer = fichier.read();
+                    fragment = fragment + buffer.v;
+                    index = fichier.getIndex()-i*nbfragments;
+                }
+                // il se trouve nécessaire de réinitialiser le buffer à une valeur non nulle du fait que
+                // la condition d'entrer dans la boucle while est que buffer != null
+                
+                int t = i%nbServers;
+                
+                Socket socket = new Socket (nomMachines[t], numPorts[t]);
+                ObjectOutputStream objectOS = new ObjectOutputStream(socket.getOutputStream());
+                objectOS.writeObject(Commande.CMD_WRITE.toString() + "/@/" + localFSSourceFname + "_" + Integer.toString(i) + "/@/" + fragment);
+                objectOS.close();
+                socket.close();
+            }
+            fichier.close();
+        }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void HdfsRead(String hdfsFname, String localFSDestFname) {
+        File file = new File(localFSDestFname + "-res");
+        try {
+        	int j;
+            FileWriter fWrite = new FileWriter(file);
+            for (int i = 0; i < nbfragments; i++) {
+            	j = i % nbServers;
+                Socket socket = new Socket (nomMachines[j], numPorts[j]);
+                ObjectOutputStream objectOS = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream objectIS = new ObjectInputStream(socket.getInputStream());
+                objectOS.writeObject(Commande.CMD_READ.toString() + "/@/" + hdfsFname + Integer.toString(i)+ "-res");
+                String fragment = (String) objectIS.readObject();
+                fWrite.write(fragment,0,fragment.length());
+                objectOS.close();
+                objectIS.close();
+                socket.close();
+                fWrite.close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+	
+    public static void main(String[] args) {
+        // java HdfsClient <read|write> <line|kv> <file>
+
+        try {
+            if (args.length<2) {usage(); return;}
+
+            switch (args[0]) {
+              case "read": HdfsRead(args[1],null); break;
+              case "delete": HdfsDelete(args[1]); break;
+              case "write": 
+                Format.Type fmt;
+                if (args.length<3) {usage(); return;}
+                if (args[1].equals("line")) fmt = Format.Type.LINE;
+                else if(args[1].equals("kv")) fmt = Format.Type.KV;
+                else {usage(); return;}
+                HdfsWrite(fmt,args[2],1);
+            }	
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
+
+// un fichier node dans lequel en écrit les noms des fichiers sur lesquels on 
+// travaille et puis le nombre de fragments. ce fichier sera en format Key 
+// value avec le signe ":" séparant les deux parties. il sera édité pendant 
+// la phase de création et de suppression des fragments.
+

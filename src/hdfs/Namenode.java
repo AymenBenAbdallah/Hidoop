400f28d23589ee78106753735a35596f53fac18b
AymenBenAbdallah <aymen.benabdallah9@live.fr>
2020-01-22 15:21:38 +0100

    node client 2.1

--- src/hdfs/Namenode.java
+++ src/hdfs/Namenode.java
@@ -1,39 +1,66 @@
 package hdfs;
 
 import java.io.*;
+import java.util.HashMap;
 
+@SuppressWarnings("serial")
 public class Namenode implements Serializable {
 
-	private HashMap<String,int> fichiers;
+	private HashMap<String,Integer> fichiers;
 	private static String nameNode = "../config/node.txt";
 	
-	public Namenode() {
-		FileInputStream fichier = new FileInputStream(nameNode);
-		ObjetInputStream objet = new ObjectInputStream(fichier);
-		this.HashMap = objectIn.readObject();
-		objet.close();
+	@SuppressWarnings("unchecked")
+	public  Namenode() throws IOException, ClassNotFoundException {
+		try {
+			FileInputStream fichier = new FileInputStream(nameNode);
+			ObjectInputStream objet = new ObjectInputStream(fichier);
+			fichiers = (HashMap<String, Integer>) objet.readObject();
+			objet.close();
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		} catch(ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
 	}
-	public int getNbFragments(String NomFichier) {
-		FileInputStream fichier = new FileInputStream(nameNode);
-		ObjetInputStream objet = new ObjectInputStream(fichier);
-		this.HashMap = objectIn.readObject();
-		objet.close();
+	@SuppressWarnings("unchecked")
+	public int getNbFragments(String NomFichier) throws IOException, ClassNotFoundException {
+		try {
+			FileInputStream fichier = new FileInputStream(nameNode);
+			ObjectInputStream objet = new ObjectInputStream(fichier);
+			this.fichiers = (HashMap<String, Integer>) objet.readObject();
+			objet.close();
+		} catch(IOException ex) {
+			ex.printStackTrace();
+			return -1;
+		} catch(ClassNotFoundException ex) {
+			ex.printStackTrace();
+			return -1;
+		}
 		return this.getNbFragments(NomFichier);
 		
 	}
-	public void Ajouter(String NomFichier, int NbFragment) {
-		this.fichiers.put(NomFichier,NbFragment);
-		FileOutputStream fichier = new FileOutputStream(nameNode);
-		ObjetOutputStream objet = new ObjectOutputStream(fichier);
-		objet.writeObject(this.fichiers);
-		objet.close();
+	public void Ajouter(String NomFichier, int NbFragment) throws IOException {
+		try {
+			this.fichiers.put(NomFichier,NbFragment);
+			FileOutputStream fichier = new FileOutputStream(nameNode);
+			ObjectOutputStream objet = new ObjectOutputStream(fichier);
+			objet.writeObject(this.fichiers);
+			objet.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
-	public void Enlever(String NomFichier) {
-		this.remove(NomFichier);
-		FileOutputStream fichier = new FileOutputStream(nameNode);
-		ObjetOutputStream objet = new ObjectOutputStream(fichier);
-		objet.writeObject(this.fichiers);
-		objet.close();
+	public void Enlever(String NomFichier) throws IOException {
+		try {
+			this.fichiers.remove(NomFichier);
+			FileOutputStream fichier = new FileOutputStream(nameNode);
+			ObjectOutputStream objet = new ObjectOutputStream(fichier);
+			objet.writeObject(this.fichiers);
+			objet.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
+

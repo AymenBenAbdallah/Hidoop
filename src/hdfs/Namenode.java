a61c853a5c5a2c41eb960fe7dc490c7132ab4256
AliAITBELAISSAOUI <57939088+AliAITBELAISSAOUI@users.noreply.github.com>
2020-01-22 15:07:56 +0100

    Update Namenode.java

--- src/hdfs/Namenode.java
+++ src/hdfs/Namenode.java
@@ -5,22 +5,35 @@ import java.io.*;
 public class Namenode implements Serializable {
 
 	private HashMap<String,int> fichiers;
-	private String nomNode;
+	private static String nameNode = "../config/node.txt";
 	
-	public Namenode(String nameNode) {
-		FileOutputStream 
-		fichiers = new HashMap<String, int>();
-		NomNode = nameNode;
+	public Namenode() {
+		FileInputStream fichier = new FileInputStream(nameNode);
+		ObjetInputStream objet = new ObjectInputStream(fichier);
+		this.HashMap = objectIn.readObject();
+		objet.close();
 	}
 	public int getNbFragments(String NomFichier) {
+		FileInputStream fichier = new FileInputStream(nameNode);
+		ObjetInputStream objet = new ObjectInputStream(fichier);
+		this.HashMap = objectIn.readObject();
+		objet.close();
 		return this.getNbFragments(NomFichier);
 		
 	}
 	public void Ajouter(String NomFichier, int NbFragment) {
-		this.put(NomFichier,NbFragment);
+		this.fichiers.put(NomFichier,NbFragment);
+		FileOutputStream fichier = new FileOutputStream(nameNode);
+		ObjetOutputStream objet = new ObjectOutputStream(fichier);
+		objet.writeObject(this.fichiers);
+		objet.close();
 	}
 	public void Enlever(String NomFichier) {
 		this.remove(NomFichier);
+		FileOutputStream fichier = new FileOutputStream(nameNode);
+		ObjetOutputStream objet = new ObjectOutputStream(fichier);
+		objet.writeObject(this.fichiers);
+		objet.close();
 	}
 	
 }

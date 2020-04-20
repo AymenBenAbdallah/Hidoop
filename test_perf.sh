66e74d755c70a998e1ee49c5944b1338ce623766
steveongit <stevebarnes888@gmail.com>
2020-04-20 15:19:05 +0200

    Modif affichage du script de tests de perfs

--- test_perf.sh
+++ test_perf.sh
@@ -13,9 +13,13 @@ DATE='/bin/date'
 DEBUT=$($DATE +'%s')
 echo $'\n## Début de l\'exécution ##\n'
 
+echo $'\n# Arret de HDFS #\n'
 sh arret_hdfs.sh
+echo $'\n# Arret de Hidoop #\n'
 sh arret_hidoop.sh
+echo $'\n# Déploiement de HDFS #\n'
 sh deploiement_hdfs.sh
+echo $'\n# Déploiement de Hidoop #\n'
 sh deploiement_hidoop.sh
 
 FIN=$($DATE +'%s')

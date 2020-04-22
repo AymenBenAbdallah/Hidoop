2049a56730696b2a4f7d54f107c61ee7853f1aa4
steveongit <stevebarnes888@gmail.com>
2020-04-22 16:55:07 +0200

    Scripts de deploiement et arret : lecture du fichier de config implémentée

--- reinisia.sh
+++ reinisia.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ssh sodium rm -rf /tmp/data
-ssh leia rm -rf /tmp/data
-ssh tao rm -rf /tmp/data
-ssh goldorak rm -rf /tmp/data
+# récupérer la 2e ligne du fichier de config (noms des machines)
+listepc=$(sed "2q;d" src/config/config_hidoop.cfg)
+
+# découper la chaîne obtenue sur les délimiteurs ","
+# et la stocker dans un tableau
+IFS=',' read -ra tabpc <<< "$listepc"
+
+# Supprimer les dossiers contenant les fragments et données hdfs + hidoop
+for pc in "${tabpc[@]}"
+do
+    ssh $pc rm -rf /tmp/data
+done
